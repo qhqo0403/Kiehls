9de719e14ea7629a33a33d3d3b8bcc8bd791082c
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-05-09 15:00:27 +0900

    main slider test

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  speed: 800,
+  spaceBetween: 0,
+
+  autoplay: {
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    clickable: true,
+  },
+
+/*   navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  }, */
+});
