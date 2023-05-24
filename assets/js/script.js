e5f0dece044642f5013bd45ce99831361b142402
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-05-24 23:25:52 +0900

    search expand and gnb toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/*
+// 메인 슬라이더 스와이퍼
 const main_swiper = new Swiper('.main', {
   direction: 'horizontal',
   loop: true,
@@ -21,7 +23,7 @@ const main_swiper = new Swiper('.main', {
     prevEl: '.swiper-button-prev',
   },
 });
-
+// 베스트 셀러 스와이퍼
 const best_swiper = new Swiper(".best", {
   slidesPerView: 4,
   spaceBetween: 40,
@@ -38,7 +40,7 @@ const best_swiper = new Swiper(".best", {
     prevEl: '.swiper-button-prev',
   },
 });
-
+// 브랜드 스토리 스와이퍼
 const story_swiper = new Swiper(".story", {
   slidesPerView: "auto",
   spaceBetween: 50,
@@ -48,12 +50,37 @@ const story_swiper = new Swiper(".story", {
   },
   observer: true,
   observeParents: true,
-})
+}) */
 
 const anchors = document.querySelectorAll('a');
+const toggleBtn = document.querySelector('.toggle-btn');
+const gnb = document.querySelector('.gnb');
+const searchBox = document.querySelector('.search');
+const searchBtn = document.querySelector('.icon');
+const clearBtn = document.querySelector('.clear');
+const searchInput = document.querySelector('.input-frame input');
 
+// a태그 최상단 이동 방지
 anchors.forEach(anchor => {
   anchor.addEventListener('click', event => {
     event.preventDefault();
   })
+});
+
+// toggle button 누르면 gnb 보이고, 버튼 모양 변형
+toggleBtn.addEventListener('click', () => {
+  toggleBtn.classList.toggle('on');
+  gnb.classList.toggle('active');
+});
+
+// search 관련 기능
+searchBtn.addEventListener('click', () => {
+  searchBox.classList.toggle('active');
 })
+clearBtn.addEventListener('click', function() {
+  if (searchInput.value) {
+    searchInput.value = '';
+  } else {
+    searchBox.classList.remove('active');
+  }
+});
